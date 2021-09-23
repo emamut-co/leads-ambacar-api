af440e7997f8daa56b867a7b02fd530a3b20415f
Faber Vergara <fabervergara@gmail.com>
2021-09-22 21:26:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,11 @@ use PHPMailer\PHPMailer\Exception;
 require 'vendor/autoload.php';
 require 'json-response.php';
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: X-Requested-With");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
 
 $mail = new PHPMailer(true);
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
